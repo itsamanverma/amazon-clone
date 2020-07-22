0a04606ac46ff93c901a2b746499bca9131b782d
itsamanverma <amanvermame786@gmail.com>
2020-07-22 17:49:31 +0530

    work on nav bar

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,22 @@
-import React from 'react'
+import React from 'react';
 import './Header.css';
+import { Link } from 'react-router-dom';
+import SearchIcon from '@material-ui/icons/Search';
 
 function Header() {
     return (
         <nav className="header">
-            <img 
-                className="header__logo" 
-                src={require(`../../assests/amazon_PNG21.png`)} 
-                alt="" />
+            <Link to="/" >
+                <img
+                    className="header__logo"
+                    src={require(`../../assests/amazon_PNG25.png`)}
+                    alt=""
+                />
+            </Link>
+            <div className="header__search">
+                <input type="text" className="header__searchInput" />
+                <SearchIcon className="header__searchIcon" />
+            </div>
         </nav>
     )
 }
