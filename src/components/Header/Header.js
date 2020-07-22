9c06a4c42ad45484e542a8010b54326326c2a7fa
itsamanverma <amanvermame786@gmail.com>
2020-07-22 19:27:33 +0530

    work on menu

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,10 +2,24 @@ import React from 'react';
 import './Header.css';
 import { Link } from 'react-router-dom';
 import SearchIcon from '@material-ui/icons/Search';
+import MenuIcon from '@material-ui/icons/Menu';
+import IconButton from '@material-ui/core/IconButton';
 
 function Header() {
     return (
         <nav className="header">
+            {/* <Link to="/">
+                <MenuIcon className="header__menuIcon" fontSize="large" />
+            </Link> */}
+            <IconButton
+                color="inherit"
+                aria-label="open drawer"
+                onClick={handleDrawer}
+                edge="start"
+                className={clsx(classes.menuButton)}
+            >
+                <MenuIcon />
+            </IconButton>
             <Link to="/" >
                 <img
                     className="header__logo"
@@ -16,6 +30,12 @@ function Header() {
             <div className="header__search">
                 <input type="text" className="header__searchInput" />
                 <SearchIcon className="header__searchIcon" />
+            </div>
+            <div className="header__nav">
+                <Link>
+                </Link>
+                <Link></Link>
+                <Link></Link>
             </div>
         </nav>
     )
