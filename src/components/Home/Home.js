ee713ce723f016f0e1d88fa8f6800ef5d5d5b875
itsamanverma <amanvermame786@gmail.com>
2020-07-24 18:29:16 +0530

    product

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -19,43 +19,43 @@ const Home = () => {
                     image={require('../../assests/pro1.jpg')}
                 />
                 <Product
-                    id="pro1"
-                    title="SKETCHFAB Extra Bass 2.0 On-Ear Headphones with Tangle Free Cable, 3.5mm Jack, Headset with Mic for Phone Calls."
-                    price={11.96}
-                    rating={5}
-                    image={require('../../assests/pro1.jpg')}
+                    id="pro2"
+                    title="Apple Watch Series 5. The most advanced Apple Watch yet, featuring the Always-On Retina display."
+                    price={50.96}
+                    rating={4}
+                    image={require('../../assests/pro8.jpg')}
                 />
             </div>
             <div className="home__row">
                 <Product
-                    id="pro1"
-                    title="SKETCHFAB Extra Bass 2.0 On-Ear Headphones with Tangle Free Cable, 3.5mm Jack, Headset with Mic for Phone Calls."
-                    price={11.96}
-                    rating={5}
-                    image={require('../../assests/pro1.jpg')}
+                    id="pro3"
+                    title="Woodlab Furniture Sheesham Wood Armchairs Outdoor Sofa Chairs for Living Room Dining Chiar for Home."
+                    price={14.96}
+                    rating={3}
+                    image={require('../../assests/pro3.jpg')}
                 />
                 <Product
-                    id="pro1"
-                    title="SKETCHFAB Extra Bass 2.0 On-Ear Headphones with Tangle Free Cable, 3.5mm Jack, Headset with Mic for Phone Calls."
-                    price={11.96}
-                    rating={5}
-                    image={require('../../assests/pro1.jpg')}
+                    id="pro4"
+                    title="Hero Kyoto 26T Single Speed Mountain Bike (Black, Ideal For : 12+ Years )."
+                    price={40.96}
+                    rating={4}
+                    image={require('../../assests/pro4.jpg')}
                 />
                 <Product
-                    id="pro1"
+                    id="pro5"
                     title="SKETCHFAB Extra Bass 2.0 On-Ear Headphones with Tangle Free Cable, 3.5mm Jack, Headset with Mic for Phone Calls."
-                    price={11.96}
-                    rating={5}
-                    image={require('../../assests/pro1.jpg')}
+                    price={20.56}
+                    rating={4}
+                    image={require('../../assests/pro5.jpg')}
                 />
             </div>
             <div className="home__row">
                 <Product
-                    id="pro1"
+                    id="pro6"
                     title="SKETCHFAB Extra Bass 2.0 On-Ear Headphones with Tangle Free Cable, 3.5mm Jack, Headset with Mic for Phone Calls."
-                    price={11.96}
-                    rating={5}
-                    image={require('../../assests/pro1.jpg')}
+                    price={800.66}
+                    rating={4}
+                    image={require('../../assests/pro6.jpg')}
                 />
             </div>
         </div>
