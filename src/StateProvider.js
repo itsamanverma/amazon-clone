49e8477bd14cb1d13808992c618851769d918591
itsamanverma <amanvermame786@gmail.com>
2020-07-25 16:45:24 +0530

    state management in whole provider

--- src/StateProvider.js
+++ src/StateProvider.js
@@ -6,8 +6,12 @@ import React, { createContext, useContext, useReducer } from "react";
 // This is the data layer 
 export const StateContext = createContext();
 
+//this a provider
 export const StateProvider = ({ reducer, initialState, children }) => (
     <StateContext.Provider value={useReducer(reducer, initialState)}>
         {children}
     </StateContext.Provider>
-)
+);
+
+//this is how we use it inside the component
+export const useStateValue = () => useContext(StateContext);
