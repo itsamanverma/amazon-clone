4eceee188fa62db502c94305947cec19626b9c35
itsamanverma <amanvermame786@gmail.com>
2020-07-27 10:33:36 +0530

    working on subtotal component design

--- src/components/Subtotal/Subtotal.js
+++ src/components/Subtotal/Subtotal.js
@@ -3,18 +3,10 @@ import './Subtotal.css';
 import Button from '@material-ui/core/Button';
 import CurrencyFormat from 'react-currency-format'
 import { useStateValue } from '../../StateProvider';
-// import FormControlLabel from '@material-ui/core/FormControlLabel';
-// import Checkbox from '@material-ui/core/Checkbox';
-// import Favorite from '@material-ui/icons/Favorite';
-// import FavoriteBorder from '@material-ui/icons/FavoriteBorder';
 
 const Subtotal = () => {
 
     const [{ basket }, dispatch] = useStateValue();
-
-    const getBasketTotal = (basket) => {
-
-    }
 
     return (
         <div className='subtotal'>
@@ -28,12 +20,6 @@ const Subtotal = () => {
                             <input 
                                 type="checkbox" 
                             /> this order contains a gift
-                            {/* <FormControlLabel
-                                control={<Checkbox icon={<FavoriteBorder />} 
-                                checkedIcon={<Favorite />} name="checked" />}
-                                label="This Order Contains a gift"
-                                className="subtotal__checkbox"
-                            /> */}
                         </small>
                     </>
                 )}
