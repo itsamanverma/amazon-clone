5f99276a116389f381196991e812e6e29417011f
itsamanverma <amanvermame786@gmail.com>
2020-09-11 23:14:12 +0530

    payment  page

--- src/components/Payment/Payment.js
+++ src/components/Payment/Payment.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './Payment.css';
 import { useStateValue } from '../../StateProvider';
+import CheckoutProduct from '../CheckoutProduct/CheckoutProduct';
 
-const Payment = () => { 
+const Payment = () => {
 
-    const [{basket,user}, dispatch] = useStateValue();
+    const [{ basket, user }, dispatch] = useStateValue();
 
     return (
         <div className="payment">
@@ -23,12 +24,32 @@ const Payment = () => {
 
                 {/* payment section - Reviews Items  */}
                 <div className="payment__section">
-
+                    <div className="payment__title">
+                        <h3>Review items and delivery</h3>
+                    </div>
+                    <div className="payment__items">
+                        {/* product items */}
+                        {basket.map((item, index) => (
+                            <CheckoutProduct key={index}
+                                id={item.id}
+                                title={item.title}
+                                price={item.price}
+                                rating={item.rating}
+                                image={item.image}
+                            />
+                        ))}
+                    </div>
                 </div>
 
                 {/* payment section - Payment method  */}
                 <div className="payment__section">
-
+                    <div className="payment__title">
+                        <h3>Payment Method</h3>
+                    </div>
+                    <div className="payment__details">
+                        {/* Stricp for payment details */}
+                        
+                    </div>
                 </div>
             </div>
         </div>
