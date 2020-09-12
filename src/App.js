4e2201ce886e8f863fdb5741a54ab13edf804801
itsamanverma <amanvermame786@gmail.com>
2020-09-12 23:38:14 +0530

    add payment functionality

--- src/App.js
+++ src/App.js
@@ -12,10 +12,14 @@ import Login from './pages/Login/Login';
 import Payment from './components/Payment/Payment';
 import { useStateValue } from './StateProvider';
 import { auth } from './firebase';
+import { loadStripe } from '@stripe/stripe-js';
+import { Elements } from '@stripe/react-stripe-js';
+
+const promise = loadStripe("pk_test_51HQbdZAxFXfllBdK2a4aRUjx0gZdVkamCLibSXushOmmwgjfKQb7DeUPvB4lf9G9bDuxbAJuzRR5ILNOFzqUQK9t00pVwCvBso");
 
 function App() {
 
-  const [{user}, dispatch] = useStateValue();
+  const [{ user }, dispatch] = useStateValue();
   /* using the useEffect concept */
   /*  */
 
@@ -40,7 +44,7 @@ function App() {
   }, [dispatch]);
 
   console.log(user);
-  
+
 
   return (
     <Router>
@@ -52,7 +56,9 @@ function App() {
           </Route>
           <Route path="/payment">
             <Header />
-            <Payment />
+            <Elements stripe={promise}>
+              <Payment />
+            </Elements>
           </Route>
           <Route path="/login">
             <Login />
